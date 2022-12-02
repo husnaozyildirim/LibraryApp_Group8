7e3b8c29700469b4cb440230dbe144d5f225727f
Husna <ozyildirimhusna@gmail.com>
2022-12-01 20:53:40 -0500

    DB connection

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_defs",
         dryRun = false,
-        tags = "@us3"
+        tags = ""
 )
 public class CukesRunner {
 
