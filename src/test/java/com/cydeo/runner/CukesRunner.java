600f22fe90fc2d308e085e6d527d16d2781693df
Husna <ozyildirimhusna@gmail.com>
2022-12-01 21:43:59 -0500

    plugin added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -7,13 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {
-                "html:target/cucumber-report.html",
+        plugin = {   "json:target/cucumber.json" ,
                 "junit:target/junit/junit-report.xml",
-                "rerun:target/rerun.txt" ,
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json" ,
-        },
+                "html:target/cucumber-report.html" ,
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        } ,
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_defs",
         dryRun = false,
